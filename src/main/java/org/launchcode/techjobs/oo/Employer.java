ae3bfedcea2275dfd9e14bbb0c9efdf471a77c27
jpbarragan <jpbarragann@gmail.com>
2023-09-29 13:41:19 -0400

    Finished DRYing code. Completed GA2

--- src/main/java/org/launchcode/techjobs/oo/Employer.java
+++ src/main/java/org/launchcode/techjobs/oo/Employer.java
@@ -4,28 +4,11 @@ import java.util.Objects;
 
 public class Employer extends JobField {
 
-    //private int id;
-    //private static int nextId = 1;
-    //private String value;
-
-    //public Employer() {
-      //  id = nextId;
-        //nextId++;
-    //}
-
     public Employer(String value) {
-      //  this();
-        //this.value = value;
         super(value);
     }
 
     // Custom toString, equals, and hashCode methods:
-
-    //@Override
-    //public String toString() {
-    //    return value;
-    //}
-
     @Override
     public boolean equals(Object o) {  // Two objects are equal if they have the same id.
         if (this == o) return true;
@@ -33,24 +16,5 @@ public class Employer extends JobField {
         Employer employer = (Employer) o;
         return getId() == employer.getId();
     }
-
-    //@Override
-    //public int hashCode() {
-    //    return Objects.hash(getId());
-    //}
-
-    // Getters and Setters:
-
-    //public int getId() {
-        //return id;
-    //}
-
-    //public String getValue() {
-    //    return value;
-    //}
-
-    //public void setValue(String value) {
-    //    this.value = value;
-    //}
 
 }
--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -4,29 +4,16 @@ import java.util.Objects;
 
 public class PositionType extends JobField {
 
-//    private int id;
-//    private static int nextId = 1;
-//    private String value;
-
-//    public PositionType() {
-//        id = nextId;
-//        nextId++;
-//    }
-
     public PositionType(String value) {
-//        this();
-//        this.value = value;
         super(value);
     }
 
     // TODO: Add a custom toString() method that returns the data stored in 'value'.
-//    @Override
-//    public String toString() {
-//        return value;
-//    }
+    // ELIMINATED AFTER DRYING CODE
 
     // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
     //  their id fields match.
+    // ELIMINATED AFTER DRYING CODE
 
     @Override
     public boolean equals(Object o) {
@@ -36,24 +23,5 @@ public class PositionType extends JobField {
         return id == that.id;
     }
 
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(id);
-//    }
-
-
-    // Getters and Setters:
-
-//    public int getId() {
-//        return id;
-//    }
-//
-//    public String getValue() {
-//        return value;
-//    }
-//
-//    public void setValue(String value) {
-//        this.value = value;
-//    }
 
 }
